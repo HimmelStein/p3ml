ca3a6b399d284fb7528b67dd4c41b8f6f05ed661
Tiansi Dong <tian1shi2@gmail.com>
2018-03-29 10:29:10 +0200

    create the prerequisites.sh script

---
+++ script_prerequisites.sh
@@ -0,0 +1,12 @@
+sudo sed -Ei 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/g' /etc/gai.conf
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential cmake gfortran git pkg-config 
+sudo apt-get install gcc-5 g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 60
+gcc --version
+echo 'export CXX=/usr/bin/g++-5' >> ~/.bashrc
+echo 'export CC=/usr/bin/gcc-5' >> ~/.bashrc
+source ~/.bashrc
+
