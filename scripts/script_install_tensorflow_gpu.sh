e0c7a07770b8293df191a14817062e73e534bbd5
Tiansi Dong <tian1shi2@gmail.com>
2018-04-06 17:41:46 +0200

    list all versions of tensorflow version

--- scripts/script_install_tensorflow_gpu.sh
+++ scripts/script_install_tensorflow_gpu.sh
@@ -1,1 +1,2 @@
+curl -s https://storage.googleapis.com/tensorflow |xmllint --format - |grep whl
 pip3 install https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0-cp34-cp34m-linux_x86_64.whl
