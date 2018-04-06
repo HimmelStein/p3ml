3c39cf762508bfe3cf9922c1b2b84ad863013a1f
Tiansi Dong <tian1shi2@gmail.com>
2018-04-06 12:47:20 +0200

    install cuda version 7.5

--- script_install_cuda.sh
+++ script_install_cuda.sh
@@ -1,23 +1,15 @@
-wget  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1704-9-1-local_9.1.85-1_amd64 -P ~/Downloads/
-sudo dpkg -i ~/Downloads/cuda-repo-ubuntu1704-9-1-local_9.1.85-1_amd64
-sudo apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub
+sudo apt-get install -y libcupti-dev
+
+wget  http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1504-7-5-local_7.5-18_amd64.deb -P ~/Downloads/
+sudo dpkg -i cuda-repo-ubuntu1504-7-5-local_7.5-18_amd64.deb
 sudo apt-get update
 sudo apt-get install -y cuda
+sudo apt-get install -y nvidia-cuda-toolkit
 
-wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/1/cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64 -P ~/Downloads/
-sudo dpkg -i ~/Downloads/cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-1_1.0-1_amd64
-sudo apt-get update
 
-wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/2/cuda-repo-ubuntu1704-9-1-local-compiler-update-1_1.0-1_amd64 -P ~/Downloads/
-sudo dpkg -i ~/Downloads/cuda-repo-ubuntu1704-9-1-local-compiler-update-1_1.0-1_amd64 
-
-wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/3/cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-3_1.0-1_amd64 -P ~/Downloads/
-sudo dpkg -i ~/Downloads/cuda-repo-ubuntu1704-9-1-local-cublas-performance-update-3_1.0-1_amd64
-
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-9.1/lib64:/usr/local/cuda-9.1/extras/CUPTI/lib64"' >> ~/.bashrc
-echo 'export CUDA_HOME=/usr/local/cuda-9.1' >> ~/.bashrc
-echo 'export PATH="/usr/local/cuda-9.1/bin:$PATH"' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda-7.5/lib64:/usr/local/cuda-9.1/extras/CUPTI/lib64"' >> ~/.bashrc
+echo 'export CUDA_HOME=/usr/local/cuda-7.5' >> ~/.bashrc
+echo 'export PATH="/usr/local/cuda-7.5/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 nvcc -V
-
